b749088700c7c680168245302e8cf854f9087903
HappyTailzHome <happytailzhome@gmail.com>
2026-04-21 09:44:43 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,36 @@
-const CACHE = "pet-app-v1";
+const CACHE = "petcare-v4";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
 self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open(CACHE).then(cache => {
-            return cache.addAll(["index.html"]);
-        })
-    );
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(networkRes => {
+        return caches.open(CACHE).then(cache => {
+          cache.put(e.request, networkRes.clone());
+          return networkRes;
+        });
+      });
+    })
+  );
 });
